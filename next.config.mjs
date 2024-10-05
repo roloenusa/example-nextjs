0e97647a1616ff51c6d6040466db8167243c08d5
Juan Delgado <roloenusa@msn.com>
2024-10-05 14:52:49 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  trailingSlash: true, // Ensures URLs end with a slash (good for static hosting)
+  // If using basePath for Amplify domain path
+  assetPrefix: './',
+};
 
 export default nextConfig;
